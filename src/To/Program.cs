ff14c832745efb99f67761cc103426e04c90f34a
Andrew <andrewmjordan@users.noreply.github.com>
2021-11-16 10:52:01 -0800

    wip: config files

--- src/To/Program.cs
+++ src/To/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 class Program
 {
@@ -9,8 +11,34 @@ class Program
     {
         var query = string.Join(" ", tokens);
 
-        var url = "https://google.com";
-        OpenBrowser(url);
+        if (!TryLaunch(query))
+        {
+            Console.Error.WriteLine($"No results for '{query}'");
+        }
+    }
+
+    public static bool TryLaunch(string query)
+    {
+        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "hotspots.csv");
+        var content = File.ReadAllLines(path);
+        foreach (var line in content)
+        {
+            try
+            {
+                var values = line.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                var name = values[0];
+                var url = values[1];
+
+                if (name == query)
+                {
+                    OpenBrowser(url);
+                    return true;
+                }
+            }
+            catch { }
+        }
+
+        return false;
     }
 
     public static void OpenBrowser(string url)
