c9c7cfe146e755c45e043eb47b658103ca72fbbb
Andrew <andrewmjordan@users.noreply.github.com>
2021-11-16 09:44:34 -0800

    setup cli

--- src/To/Program.cs
+++ src/To/Program.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 class Program
 {
 
-	static void Main(string[] args)
-	{
-		Console.WriteLine("Hello world!");
-	}
+    static void Main(string[] tokens)
+    {
+        var query = string.Join(" ", tokens);
+
+        var url = "https://google.com";
+        OpenBrowser(url);
+    }
+
+    public static void OpenBrowser(string url)
+    {
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
 }
